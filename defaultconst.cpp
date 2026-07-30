7214c9bcf37daff06609de1dd3be782aeb5e4457
ShwetaB <badadeshwetav@gmail.com>
2026-07-30 23:40:09 +0530

    added default constructor program

---
+++ defaultconst.cpp
@@ -0,0 +1,18 @@
+//default constructor program
+#include<iostream>
+using namespace std;
+class Base
+{
+	public: 
+	Base()
+	{
+	cout<<"default constructor";
+	}
+};
+
+
+int main()
+{
+	Base d;
+	
+}
