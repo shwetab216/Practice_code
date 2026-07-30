2dcbf7d1c6fb52da90fa52ab30ee35b742ea12e4
ShwetaB <badadeshwetav@gmail.com>
2026-07-30 23:19:13 +0530

    Single inheritance program

---
+++ singleinheritance.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class Base
+{
+	public: 
+	void shape()
+	{
+		cout<<"Base method";
+	}
+};
+
+class Derived : public Base
+{
+    public:
+	void shape()
+	{
+		cout<<"Derived method";
+	}
+	
+};
+
+int main()
+{
+	Derived d;
+	d.shape();
+}
